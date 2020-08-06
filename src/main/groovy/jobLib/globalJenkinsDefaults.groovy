9b79468c80bf0f702228d610562716946defb336
Kedar Vijay Kulkarni <kkulkarni@redhat.com>
2020-08-06 10:32:42 -0400

    RFR: Adding Regular SLA Check Job

--- src/main/groovy/jobLib/globalJenkinsDefaults.groovy
+++ src/main/groovy/jobLib/globalJenkinsDefaults.groovy
@@ -4,6 +4,7 @@ class globalJenkinsDefaults {
     static String gitlab_url = "https://gitlab.sat.engineering.redhat.com/satelliteqe/satelliteqe-jenkins.git"
     static String master_branch = "*/master"
     static String git_creds = "satqe_gitlab_jenkins_token"
+    static String tower_url = "https://infra-ansible-tower-01.infra.sat.rdu2.redhat.com"
     static String tower_prod = "Infra-Ansible-Tower-01"
     static String tower_user = "ansible-tower-jenkins-user"
 
