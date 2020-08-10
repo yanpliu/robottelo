3f9f2e72f08d8f257f9ef3ab3bed3cfd85aca34c
Mike Shriver <mshriver@redhat.com>
2020-08-10 10:23:07 -0400

    Update pipeline script
    Fix whitespace formatting and indention
    
    JSON parsing and file writing has some issues when it comes to object types, using readJSON instead of JsonSlurper.
    
    https://issues.jenkins-ci.org/browse/JENKINS-44587

--- src/resources/snapTemplateCreationPipeline.groovy
+++ src/resources/snapTemplateCreationPipeline.groovy
@@ -3,134 +3,136 @@
 import groovy.json.*
 
 withCredentials([usernamePassword(credentialsId: 'ansible-tower-jenkins-user', passwordVariable: 'USERPASS', usernameVariable: 'USERNAME')]) {
-        def at_vars = [
-                containerEnvVar(key: 'DYNACONF_AnsibleTower__base_url', value: "${params.tower_url}"),
-                containerEnvVar(key: 'DYNACONF_AnsibleTower__username', value: "${USERNAME}"),
-                containerEnvVar(key: 'DYNACONF_AnsibleTower__password', value: "${USERPASS}")
-        ]
+    def at_vars = [
+        containerEnvVar(key: 'DYNACONF_AnsibleTower__base_url', value: "${params.tower_url}"),
+        containerEnvVar(key: 'DYNACONF_AnsibleTower__username', value: "${USERNAME}"),
+        containerEnvVar(key: 'DYNACONF_AnsibleTower__password', value: "${USERPASS}")
+    ]
 
-        openShiftUtils.withNode(image: pipelineVars.ciBrokerImage, envVars: at_vars) {
+    openShiftUtils.withNode(image: pipelineVars.ciBrokerImage, envVars: at_vars) {
 
-            stage('Parse UMB Message') {
-                        println("CI Event Received, parsing message")
+        stage('Parse UMB Message') {
+            println("CI Event Received, parsing message")
 
-                        // Read CI message from build environment
-                        def message = readJSON text: params.get("CI_MESSAGE")
+            // Read CI message from build environment
+            def message = readJSON text: params.get("CI_MESSAGE")
 
-                        println("CI Message: " + JsonOutput.prettyPrint(JsonOutput.toJson(message)));
+            println("CI Message: " + JsonOutput.prettyPrint(JsonOutput.toJson(message)));
 
-                        // Write CI message to file to be archived
-                        writeJSON file: "ci-message.json", json: message
+            // Write CI message to file to be archived
+            writeJSON file: "ci-message.json", json: message
 
-                        sat_version = message.get('satellite_version', '')
-                        capsule_version = sat_version
-                        snap_version = message.get('snap_version', '')
-                        rhel_major_version = message.get('rhel_major_version', '')
-                        satellite_activation_key = message.get('satellite_activation_key', '')
-                        capsule_activation_key = message.get('capsule_activation_key', '')
+            sat_version = message.get('satellite_version', '')
+            capsule_version = sat_version
+            snap_version = message.get('snap_version', '')
+            rhel_major_version = message.get('rhel_major_version', '')
+            satellite_activation_key = message.get('satellite_activation_key', '')
+            capsule_activation_key = message.get('capsule_activation_key', '')
 
-                        println("sat_version is " + sat_version)
-                        println("snap_version is " + snap_version)
-                        println("rhel_major_version is " + rhel_major_version)
-                        println("capsule_version is " + capsule_version)
-                        println("satellite_activation_key " + satellite_activation_key)
-                        println("capsule_activation_key is " + capsule_activation_key)
+            println("sat_version is " + sat_version)
+            println("snap_version is " + snap_version)
+            println("rhel_major_version is " + rhel_major_version)
+            println("capsule_version is " + capsule_version)
+            println("satellite_activation_key " + satellite_activation_key)
+            println("capsule_activation_key is " + capsule_activation_key)
 
-                        // Check for any value not set
-                        if (sat_version && snap_version && capsule_version && rhel_major_version && satellite_activation_key && capsule_activation_key) {
-                            print "All Work flow values have been set"
-                        } else {
-                            error("One or more variables were empty")
-                        }
-
-            }
-
-            stage('Create Snap Templates ') {
-
-                    // Call all three AT snap template workflows in parallel
-                    parallel (
-                        "create-sat-jenkins-template" : {
-                            output_sat_jenkins =
-                                sh (
-                                    returnStdout: true,
-                                    script:
-                                         """
-                                            broker execute --workflow 'create-sat-jenkins-template' \
-                                            --output-format raw --artifacts last --additional-arg True \
-                                            --activation_key ${satellite_activation_key}\
-                                            --rhel_major_version ${rhel_major_version} \
-                                            --sat_version ${sat_version} \
-                                            --snap_version ${snap_version}
-                                        """
-                                )
-                        },
-                        "create-sat-lite-template" : {
-                            output_sat_lite =
-                                sh (
-                                    returnStdout: true,
-                                    script:
-                                         """
-                                            broker execute --workflow 'create-sat-lite-template' \
-                                            --output-format raw --artifacts last --additional-arg True \
-                                            --activation_key ${satellite_activation_key}\
-                                            --rhel_major_version ${rhel_major_version} \
-                                            --sat_version ${sat_version} \
-                                            --snap_version ${snap_version}
-                                        """
-                                )
-                        },
-                         "create-capsule-template" : {
-                             output_capsule =
-                                 sh (
-                                     returnStdout: true,
-                                     script:
-                                         """
-                                            broker execute --workflow 'create-capsule-template' \
-                                            --output-format raw --artifacts last --additional-arg True \
-                                            --activation_key ${capsule_activation_key}\
-                                            --rhel_major_version ${rhel_major_version} \
-                                            --sat_version ${sat_version} \
-                                            --snap_version ${snap_version}
-                                        """
-                                 )
-                        },
-                    )
-            }
-
-            stage('Parse Output and Set Template Names'){
-
-                // Output to JSON
-                def jsonSlurper = new JsonSlurper()
-                // groovy data processing
-                // fixing JSON format
-                def output_sat_json = jsonSlurper.parseText(output_sat_jenkins.replace("'","\""))
-                def output_lite_json = jsonSlurper.parseText(output_sat_lite.replace("'","\""))
-                def output_capsule_json = jsonSlurper.parseText(output_capsule.replace("'","\""))
-
-                // Write CI message to file to be archived
-                writeJSON file: "sat-jenkins-temp-creation.json", json: output_sat_json
-                writeJSON file: "sat-lite-temp-creation.json", json: output_lite_json
-                writeJSON file: "capsule-temp-creation.json", json: output_capsule_json
-
-                sat_jenkins_template = output_sat_json.get('data_out', '').get('template', '')
-                sat_lite_template = output_lite_json.get('data_out', '').get('template', '')
-                capsule_template = output_capsule_json.get('data_out', '').get('template', '')
-
-                println("Sat Jenkins Template Name is " + sat_jenkins_template)
-                println("Sat Lite Template Name is " + sat_lite_template)
-                println("Sat Capsule Template Name is " + capsule_template)
-
-                // Check for any value not set
-               if (sat_jenkins_template && sat_lite_template && capsule_template) {
-                   print "All template names have been created"
-               } else {
-                   error("One or more template names were empty")
-               }
-            }
-
-            stage('Archive Artifacts'){
-                archiveArtifacts artifacts: '*.json'
+            // Check for any value not set
+            if (sat_version && snap_version && capsule_version && rhel_major_version && satellite_activation_key && capsule_activation_key) {
+                print "All Work flow values have been set"
+            } else {
+                error("One or more variables were empty")
             }
 
         }
+
+        stage('Create Snap Templates ') {
+
+            // Call all three AT snap template workflows in parallel
+            parallel (
+                "create-sat-jenkins-template" : {
+                    output_sat_jenkins =
+                        sh (
+                            returnStdout: true,
+                            script:
+                                """
+                                    broker execute --workflow 'create-sat-jenkins-template' \
+                                    --output-format raw --artifacts last --additional-arg True \
+                                    --activation_key ${satellite_activation_key}\
+                                    --rhel_major_version ${rhel_major_version} \
+                                    --sat_version ${sat_version} \
+                                    --snap_version ${snap_version}
+                                """
+                        )
+                },
+                "create-sat-lite-template" : {
+                    output_sat_lite =
+                        sh (
+                            returnStdout: true,
+                            script:
+                                """
+                                    broker execute --workflow 'create-sat-lite-template' \
+                                    --output-format raw --artifacts last --additional-arg True \
+                                    --activation_key ${satellite_activation_key}\
+                                    --rhel_major_version ${rhel_major_version} \
+                                    --sat_version ${sat_version} \
+                                    --snap_version ${snap_version}
+                                """
+                        )
+                },
+                "create-capsule-template" : {
+                    output_capsule =
+                        sh (
+                        returnStdout: true,
+                            script:
+                            """
+                                broker execute --workflow 'create-capsule-template' \
+                                --output-format raw --artifacts last --additional-arg True \
+                                --activation_key ${capsule_activation_key}\
+                                --rhel_major_version ${rhel_major_version} \
+                                --sat_version ${sat_version} \
+                                --snap_version ${snap_version}
+                            """
+                        )
+                },
+            )
+        }
+
+        stage('Parse Output and Set Template Names'){
+            // Print raw output, small JSON so not pretty printing
+            println("create-sat-jenkins-template output: " +output_sat_jenkins)
+            println("create-sat-lite-template output: " + output_sat_lite)
+            println("create-capsule-template output: " + output_capsule)
+
+            // Output to JSON files
+            // Single quotes in the broker console output, replace with double quote
+            output_sat_json = readJSON text: output_sat_jenkins.replace("'","\"")
+            output_lite_json = readJSON text: output_sat_lite.replace("'","\"")
+            output_capsule_json = readJSON text: output_capsule.replace("'","\"")
+
+            writeJSON file: "sat-jenkins-temp-creation.json", json: output_sat_json['data_out']
+            writeJSON file: "sat-lite-temp-creation.json", json: output_lite_json['data_out']
+            writeJSON file: "capsule-temp-creation.json", json: output_capsule_json['data_out']
+
+            // pull out the template name field for validation that all of them worked
+            sat_jenkins_template = output_sat_json.get('data_out', '').get('template', '')
+            sat_lite_template = output_lite_json.get('data_out', '').get('template', '')
+            capsule_template = output_capsule_json.get('data_out', '').get('template', '')
+
+            println("Sat Jenkins Template Name is " + sat_jenkins_template)
+            println("Sat Lite Template Name is " + sat_lite_template)
+            println("Sat Capsule Template Name is " + capsule_template)
+
+            // Check for any value not set
+            if (sat_jenkins_template && sat_lite_template && capsule_template) {
+                print "All template names have been created"
+            } else {
+                error("One or more template names were empty")
+            }
+        }
+
+        stage('Archive Artifacts'){
+            archiveArtifacts artifacts: '*.json'
+        }
+
+    }
 }
