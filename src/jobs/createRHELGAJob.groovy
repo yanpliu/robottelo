aa1a3a2cf178e8613f1b00be7bbc77073c192df2
Justin Watts <juwatts@redhat.com>
2020-08-27 16:11:35 -0400

    Add JobDSL and Pipeline Script for Rhel temp creation

---
+++ src/jobs/createRHELGAJob.groovy
@@ -0,0 +1,65 @@
+// Create RHEL Templates
+
+import jobLib.globalJenkinsDefaults
+
+pipelineJob('rhel-templatization') {
+
+    description('RHEL Templatization Pipeline.')
+    parameters {
+//         stringParam('CI_MESSAGE',"",'UMB message that comes from CI-trigger. It is expected to provide an artifact  .id:COMPOSE_ID, extra.target:CDN, extra.results_id:JIRA')
+//        stringParam('MESSAGE_HEADERS',"","Headers of the message that comes from CI-trigger")
+        stringParam('qcow_url',"",'URL to the QCOW image that will be templatized')
+        stringParam('rhel_version',"","RHEL Version, format is x.y ")
+        stringParam('tower_url', globalJenkinsDefaults.tower_url, "Ansible Tower URL, format 'https://<url>/'")
+    }
+
+    // PLace holder for RHEL GA UMB
+//    properties {
+//        pipelineTriggers {
+//          triggers {
+//            ciBuildTrigger {
+//              providers {
+//                providerDataEnvelope {
+//                  providerData {
+//                    activeMQSubscriber {
+//                      name("Red Hat UMB")
+//                      overrides {
+//                        topic(consumerTopic)
+//                      }
+                      // Message Checks
+                      // Keeping this for an example if we need to be more specific
+//                       checks {
+//                        msgCheck {
+//                          field('')
+//                          expectedValue('')
+//                        }
+//                      }
+//                    }
+//                  }
+//                }
+//              }
+//              noSquash(true)
+//            }
+//          }
+//        }
+//    }
+
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scm {
+                git {
+                    remote {
+                        name()
+                        url(globalJenkinsDefaults.gitlab_url)
+                        credentials(globalJenkinsDefaults.git_creds)
+                    }
+                    branch(globalJenkinsDefaults.master_branch)
+                }
+            }
+            scriptPath("src/resources/rhelGATemplateCreationPipeline.groovy")
+        }
+    }
+
+}
---
+++ src/resources/rhelGATemplateCreationPipeline.groovy
@@ -0,0 +1,67 @@
+@Library("satqe_pipeline_lib") _
+
+import groovy.json.*
+
+withCredentials([usernamePassword(credentialsId: 'ansible-tower-jenkins-user', passwordVariable: 'USERPASS', usernameVariable: 'USERNAME')]) {
+    def at_vars = [
+        containerEnvVar(key: 'DYNACONF_AnsibleTower__base_url', value: "${params.tower_url}"),
+        containerEnvVar(key: 'DYNACONF_AnsibleTower__username', value: "${USERNAME}"),
+        containerEnvVar(key: 'DYNACONF_AnsibleTower__password', value: "${USERPASS}")
+    ]
+
+    openShiftUtils.withNode(image: pipelineVars.ciBrokerImage, envVars: at_vars) {
+
+//        stage('Parse UMB Message') {
+//             Place holder for RHEL GA UMB Parsing
+//
+//        }
+
+        stage('Create RHEL GA Template') {
+
+            // Call RHEL GA Template Workflow
+            output_rhel_ga =
+                sh (
+                    returnStdout: true,
+                    script:
+                        """
+                            broker execute --workflow 'create-rhel-ga-template' \
+                            --output-format raw --artifacts last --additional-arg True \
+                            --qcow_url ${params.qcow_url}\
+                            --rhel_version ${params.rhel_version} 
+                        """
+                )
+        }
+
+        stage('Parse Output and Set Template Names'){
+
+            // Print raw output, small JSON so not pretty printing
+            println("create-rhel-ga-template output: " + output_rhel_ga)
+
+            // Output to JSON files
+            // Single quotes in the broker console output, replace with double quote
+            rhel_ga_template = null
+
+            if (output_rhel_ga.contains("data_out")) {
+                output_rhel_ga_json = readJSON text: output_rhel_ga.replace("'","\"")
+                writeJSON file: "rhel-ga-temp-creation.json", json: output_rhel_ga_json['data_out']
+                rhel_ga_template = output_rhel_ga_json.get('data_out', '').get('template', '')
+                println("RHEL GA Template Name is " + rhel_ga_template)
+            } else {
+                println("ERROR: broker call for RHEL GA workflow must have failed, nothing was returned")
+            }
+
+
+        }
+
+        stage('Archive Artifacts'){
+            archiveArtifacts artifacts: '*.json'
+
+            // Check for any value not set
+            if (rhel_ga_template) {
+                print "RHEL GA Template has been created"
+            } else {
+                error("Template name is empty")
+            }
+        }
+    }
+}
