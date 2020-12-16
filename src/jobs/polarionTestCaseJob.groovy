3e6640bceacea26380812aa78a7dae0844b05a46
Lukas Pramuk <lpramuk@redhat.com>
2020-12-16 13:33:10 +0100

    Implement Polarion test Case job

---
+++ src/jobs/polarionTestCaseJob.groovy
@@ -0,0 +1,36 @@
+// Polarion Test Case Import
+
+import jobLib.globalJenkinsDefaults
+import jenkins.model.*
+
+pipelineJob("polarion-testcase-upload") {
+    disabled(Jenkins.getInstance().getRootUrl() != globalJenkinsDefaults.production_url)
+
+    description('Polarion Test Case data upload job.')
+
+    parameters {
+        stringParam('polarion_url', globalJenkinsDefaults.polarion_url, "Polarion URL to upload Test Case data to")
+    }
+
+    properties {
+        disableConcurrentBuilds()
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scm {
+                git {
+                    remote {
+                        name()
+                        url(globalJenkinsDefaults.gitlab_url)
+                        credentials(globalJenkinsDefaults.git_creds)
+                    }
+                    branch(globalJenkinsDefaults.master_branch)
+                }
+            }
+            scriptPath("src/resources/polarionTestCasePipeline.groovy")
+        }
+    }
+
+}
