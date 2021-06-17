52a62ec45ef76a4c8822f7cc701d6953eb9d53be
omkarkhatavkar <okhatavkar007@gmail.com>
2021-06-17 14:33:05 +0530

    added missing members in pr tester tool

--- src/main/groovy/jobLib/prTesterDefaults.groovy
+++ src/main/groovy/jobLib/prTesterDefaults.groovy
@@ -6,7 +6,8 @@ class prTesterDefaults {
 static List userWhitelist = ['omkarkhatavkar', 'jyejare', 'rplevka', 'jameerpathan111', 'swadeley',
 'mshriver', 'jacobcallahan', 'latran', 'rdrazny', 'tstrych', 'lvrtelov', 'mccun934', 'ehelms', 'tpapaioa',
  'lhellebr', 'lpramuk', 'eko999', 'pondrejk', 'mirekdlugosz', 'devendra104', 'ogajduse', 'Gauravtalreja1',
- 'ColeHiggins2', 'jawatts', 'akhil-jha', 'bherrin3', 'hkx303', 'vsedmik', 'synkd']
+ 'ColeHiggins2', 'jawatts', 'akhil-jha', 'bherrin3', 'hkx303', 'vsedmik', 'synkd', 'shweta83', 'tstrych',
+ 'rmynar', 'pdragun', 'pochop', 'spusateri']
 
 // for reference https://github.com/orgs/SatelliteQE/teams/repository-admins/members
 static List admins = ['lpramuk', 'mshriver', 'jacobcallahan', 'jyejare', 'bherrin3', 'vijay8451',
