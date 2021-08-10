92675f56f1efcf2a1d7534073efee9ba089a50a8
Lukas Pramuk <lpramuk@redhat.com>
2021-08-10 19:08:00 +0200

    Add try/catch/finally to polarion-testrun-upload job

--- src/resources/polarionTestResultUploadPipeline.groovy
+++ src/resources/polarionTestResultUploadPipeline.groovy
@@ -4,47 +4,66 @@ import groovy.json.*
 
 openShiftUtils.withNode(image: "$pipelineVars.ciRobotteloImage:${pipelineVars.robotteloImageTags.find{sat_version.startsWith(it.key)}.value}") {
 
-    stage('Set Build Description') {
-        currentBuild.description = "Trigger: " + params.job_name +
-                " Run: " + params.build_number +
-                " Build: " + params.sat_version + "-" + params.snap_version + (params.test_run_type ? " $params.test_run_type" : "")
+    try {
+        stage('Set Build Description') {
+            currentBuild.description = "Trigger: " + params.job_name +
+                    " Run: " + params.build_number +
+                    " Build: " + params.sat_version + "-" + params.snap_version + (params.test_run_type ? " $params.test_run_type" : "")
+        }
 
-    }
-
-    stage('Copy Test Run Artifacts') {
-        copyArtifacts(
-                projectName: "${params.job_name}",
-                selector: specific("${params.build_number}"),
-                filter : '*results.xml',
-                target: "${WORKSPACE}"
-        )
-        sh """
-            cp ${WORKSPACE}/*results.xml \${ROBOTTELO_DIR}
-        """
-    }
-
-    stage('Set Artifact Filename') {
-        file_name = sh (
-                returnStdout: true,
-                script: """
-                find \${ROBOTTELO_DIR} -name *results.xml -printf "%f\n"
+        stage('Copy Test Run Artifacts') {
+            copyArtifacts(
+                    projectName: "${params.job_name}",
+                    selector: specific("${params.build_number}"),
+                    filter : '*results.xml',
+                    target: "${WORKSPACE}"
+            )
+            sh """
+                cp ${WORKSPACE}/*results.xml \${ROBOTTELO_DIR}
             """
-        )
-        println("Filename is ${file_name}")
+        }
+
+        stage('Set Artifact Filename') {
+            file_name = sh (
+                    returnStdout: true,
+                    script: """
+                    find \${ROBOTTELO_DIR} -name *results.xml -printf "%f\n"
+                """
+            )
+            println("Filename is ${file_name}")
+        }
+
+        stage('Test Result Upload') {
+            // Set the ID for the polarion test run
+            test_run_id = "Satellite ${sat_version}-${snap_version} rhel${rhel_version}" + (test_run_type ? " $test_run_type" : "")
+            sh """
+                cd \${ROBOTTELO_DIR}
+                pip install Betelgeuse==1.8.0
+                scripts/polarion-test-run-upload.sh \
+                    -s ${params.polarion_url} \
+                    -P ${pipelineVars.polarionProject} \
+                    -u ${pipelineVars.polarionUser} \
+                    -i '${test_run_id}' \
+                    -r ${file_name}
+            """
+        }
+    }
+    catch (exc) {
+        print "Pipeline failed with ${exc}"
+        email_to = ['sat-qe-jenkins', 'satellite-lab-list']
+        subject = "${env.JOB_NAME} Build ${BUILD_NUMBER} has Failed. Please Investigate"
+        body = "Jenkins Console Log: ${BUILD_URL}. Error that was caught:<br><br> ${exc}"
+        currentBuild.result = 'FAILURE'
+    }
+    finally {
+        if(currentBuild.result == 'FAILURE') {
+            emailUtils.sendEmail(
+                'to_nicks': email_to,
+                'reply_nicks': email_to,
+                'subject': subject,
+                'body': body
+            )
+        }
     }
 
-    stage('Test Result Upload') {
-        // Set the ID for the polarion test run
-        test_run_id = "Satellite ${sat_version}-${snap_version} rhel${rhel_version}" + (test_run_type ? " $test_run_type" : "")
-        sh """
-            cd \${ROBOTTELO_DIR}
-            pip install Betelgeuse==1.8.0
-            scripts/polarion-test-run-upload.sh \
-                -s ${params.polarion_url} \
-                -P ${pipelineVars.polarionProject} \
-                -u ${pipelineVars.polarionUser} \
-                -i '${test_run_id}' \
-                -r ${file_name}
-        """
-    }
 }
